b1de34e82b77cc5e7fa92dc6556b9d4d2731320d
Karl Larsaeus <karl@ninjacontrol.com>
2021-11-28 20:16:55 +0100

    Invoke 'bash' instead of 'sh' when starting script

--- src/test/mal/regression-test.sh
+++ src/test/mal/regression-test.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env -vS bash -x
+#!/usr/bin/env bash
 test_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 steps=(step2_eval.mal step3_env.mal step4_if_fn_do.mal step5_tco.mal step6_file.mal step7_quote.mal step8_macros.mal step9_try.mal stepA_mal.mal)
 
 for i in "${steps[@]}"
 do
-   "${test_dir}"/runtest.py  --rundir "${test_dir}" --hard --deferrable --optional "$i" -- sh "${test_dir}"/../../../bin/krisp-repl.sh
+   "${test_dir}"/runtest.py  --rundir "${test_dir}" --hard --deferrable --optional "$i" -- bash "${test_dir}"/../../../bin/krisp-repl.sh
 done
