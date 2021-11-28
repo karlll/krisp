1fd8459b661fec721a5f8c2519855ade70a8c505
Karl Larsaeus <karl@ninjacontrol.com>
2021-11-28 18:35:44 +0100

    Update dir variable in test scripts

--- bin/krisp-repl.sh
+++ bin/krisp-repl.sh
@@ -1,4 +1,4 @@
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+bin_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-java -jar "${SCRIPT_DIR}/../build/libs/krisp-0.0.1-SNAPSHOT.jar"
+java -jar "${bin_dir}/../build/libs/krisp-0.0.1-SNAPSHOT.jar"
 
--- src/test/mal/regression-test.sh
+++ src/test/mal/regression-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+test_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 steps=(step2_eval.mal step3_env.mal step4_if_fn_do.mal step5_tco.mal step6_file.mal step7_quote.mal step8_macros.mal step9_try.mal stepA_mal.mal)
 
 for i in "${steps[@]}"
 do
-   "${SCRIPT_DIR}"/runtest.py  --rundir "${SCRIPT_DIR}" --hard --deferrable --optional "$i" -- sh "${SCRIPT_DIR}"/../../../bin/krisp-repl.sh
+   "${test_dir}"/runtest.py  --rundir "${test_dir}" --hard --deferrable --optional "$i" -- sh "${test_dir}"/../../../bin/krisp-repl.sh
 done
