2680c9a34954625e6d0cf095d119a6868571893f
Karl Larsaeus <karl@ninjacontrol.com>
2021-11-28 19:41:51 +0100

    Add debug mode for regression-test.sh

--- src/test/mal/regression-test.sh
+++ src/test/mal/regression-test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 test_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 steps=(step2_eval.mal step3_env.mal step4_if_fn_do.mal step5_tco.mal step6_file.mal step7_quote.mal step8_macros.mal step9_try.mal stepA_mal.mal)
